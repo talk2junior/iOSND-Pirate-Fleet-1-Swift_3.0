64d339645f3c550152460215a433f883446376af
talk2junior <talk2junior@gmail.com>
2017-01-05 14:15:45 -0500

    Initial upload of iOSND course files
    Pirate fleet 1 files. Use for exercises and practice.

---
+++ ios-nd-pirate-fleet-1-swift-3/Pirate Fleet/ControlCenter.swift
@@ -0,0 +1,34 @@
+//
+//  ControlCenter.swift
+//  Pirate Fleet
+//
+//  Created by Jarrod Parkes on 9/2/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+struct GridLocation {
+    let x: Int
+    let y: Int
+}
+
+struct Ship {
+    let length: Int
+    let location: GridLocation
+    let isVertical: Bool
+}
+
+class ControlCenter {
+    
+    func addShipsAndMines(_ human: Human) {
+        // Write your code here!
+    }
+    
+    func calculateFinalScore(_ gameStats: GameStats) -> Int {
+        
+        var finalScore: Int
+        
+        finalScore = 0                
+        
+        return finalScore
+    }
+}
